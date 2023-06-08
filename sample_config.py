2361efc6e021d79e31ef19d1d19e6c5a11926ab0
Shashankls <125437933+Shashankls@users.noreply.github.com>
2023-06-08 16:01:29 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -24,7 +24,7 @@ class Config(object):
     TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "")
 
     # Database URI
-    DB_URI = os.environ.get("DATABASE_URL", "")
+    DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://shashankls:shashank123@cluster0.x1uuavp.mongodb.net/?retryWrites=true&w=majority")
 
 
 def LOGGER(name: str) -> logging.Logger:
